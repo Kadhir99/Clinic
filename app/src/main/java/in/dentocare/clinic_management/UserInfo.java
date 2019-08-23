e9367d884e90653f062ae634a38f0a7c05325e39
Adharsh <adharshkrish@outlook.com>
2019-08-23 09:52:13 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/drawable-hdpi/background.png
    #	app/src/main/res/drawable-ldpi/background.png
    #	app/src/main/res/drawable-mdpi/background.png
    #	app/src/main/res/drawable-xhdpi/background.png
    #	app/src/main/res/drawable-xxhdpi/background.png
    #	app/src/main/res/drawable-xxxhdpi/background.png
    #	app/src/main/res/layout/activity_login.xml

--- app/src/main/java/in/dentocare/clinic_management/UserInfo.java
+++ app/src/main/java/in/dentocare/clinic_management/UserInfo.java
@@ -1,10 +1,18 @@
 package in.dentocare.clinic_management;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import static android.app.Notification.EXTRA_NOTIFICATION_ID;
 
 public class UserInfo extends AppCompatActivity {
 
@@ -17,5 +25,40 @@ public class UserInfo extends AppCompatActivity {
         String name = getIntent().getStringExtra("name");
         nam = findViewById(R.id.name);
         nam.setText(name);
+
+        // Create an explicit intent for an Activity in your app
+        Intent intent = new Intent(this, Doctor.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+//        createNotificationChannel();
+        NotificationCompat.Builder appointment = new NotificationCompat.Builder(this,"appoint")
+                .setSmallIcon(R.drawable.appointment_notification)
+                .setContentTitle("My notification")
+                .setContentText("Much longer text that cannot fit one line...")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Much longer text that cannot fit one line can be shrunken and when expanded it show..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(10, appointment.build());
     }
+//    private void createNotificationChannel() {
+//        // Create the NotificationChannel, but only on API 26+ because
+//        // the NotificationChannel class is new and not in the support library
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            CharSequence name = getString(R.string.channel_name);
+//            String description = getString(R.string.channel_description);
+//            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+//            NotificationChannel channel = new NotificationChannel("appoint", name, importance);
+//            channel.setDescription(description);
+//            // Register the channel with the system; you can't change the importance
+//            // or other notification behaviors after this
+//            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+//            notificationManager.createNotificationChannel(channel);
+//        }
+//    }
+
 }
